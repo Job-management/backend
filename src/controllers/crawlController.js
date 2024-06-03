ea5c1c89794d3149ae213543cfae972cc16ce056
vth20 <vanthanhhuynhtk@gmail.com>
2024-06-03 22:18:46 +0700

    [api][analyze] user and crawl post

--- src/controllers/crawlController.js
+++ src/controllers/crawlController.js
@@ -33,11 +33,79 @@ const getDataCrawls = catchAsync(async (req, res) => {
       ? Number.parseInt(req.query.major_category_id)
       : "None",
     address: req.query.address ? req.query.address : "None",
-    salary_from: salary_from ? Number.parseFloat(salary_from) : 'None',
-    salary_to: salary_to ? Number.parseFloat(salary_to) : 'None'
+    salary_from: salary_from ? Number.parseFloat(salary_from) : "None",
+    salary_to: salary_to ? Number.parseFloat(salary_to) : "None",
   };
 
   const rawData = await CrawlDataService.getDataCrawls(
+    startIndex,
+    limit,
+    params
+  );
+  const data = rawData.result.map((item, index) => {
+    const _images =
+      item.images && item.images !== "None"
+        ? JSON.parse(item.images.replaceAll("None", null).replaceAll("'", '"'))
+        : null;
+    const images = Array.isArray(_images)
+      ? _images.map((item) => {
+          return {
+            ...item,
+            src: item.link || item.src,
+          };
+        })
+      : null;
+    return {
+      ...item,
+      images,
+    };
+  });
+  res
+    .status(httpStatus.OK)
+    .send(
+      successPagination(
+        "SUCCESS",
+        data,
+        httpStatus.OK,
+        rawData.total,
+        page,
+        limit
+      )
+    );
+});
+
+const getAllDataCrawls = catchAsync(async (req, res) => {
+  const page = Number.parseInt(req.query.page) || 1;
+  const limit = Number.parseInt(req.query.limit) || 20;
+
+  if (Number.isNaN(page) || page < 1 || Number.isNaN(limit) || limit < 0) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "Invalid query parameters!");
+  }
+
+  // Initialize salary_from and salary_to
+  let salary_from = null;
+  let salary_to = null;
+
+  // Check if salary query parameter is present
+  if (req.query.salary) {
+    [salary_from, salary_to] = req.query.salary.split("-");
+  }
+
+  // Calculate start index for pagination
+  const startIndex = (page - 1) * limit;
+
+  // Parameters object
+  const params = {
+    search: req.query.search ? req.query.search.trim() : "",
+    major_category_id: req.query.major_category_id
+      ? Number.parseInt(req.query.major_category_id)
+      : "None",
+    address: req.query.address ? req.query.address : "None",
+    salary_from: salary_from ? Number.parseFloat(salary_from) : "None",
+    salary_to: salary_to ? Number.parseFloat(salary_to) : "None",
+  };
+
+  const rawData = await CrawlDataService.getAllDataCrawls(
     startIndex,
     limit,
     params
@@ -133,10 +201,19 @@ const searchJob = async (search) => {
 
 const queryJob = async ({ search, major, city }) => {};
 
+const updateStatusPost = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const status = req.body.status;
+  await CrawlDataService.updateStatusPostById(id, status);
+  res.status(200).send("OK");
+});
+
 module.exports = {
   getDataCrawls,
   getDataCrawlById,
   updateJob,
   getJobByTitle,
   filterJob,
+  updateStatusPost,
+  getAllDataCrawls,
 };
